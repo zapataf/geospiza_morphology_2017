ad33d4ab36b25aff08de18c0b1648d322a8ca838
Felipe Zapata <fzapata@ucla.edu>
2017-04-25 23:11:47 -0700

    Typo

--- analysis.r
+++ analysis.r
@@ -603,9 +603,9 @@ data_pca_mclust %>%
   tally() %>%
   mutate( fraction = n / sum(n) )
 
-##############################
-## EXPORATORY DATA ANALYSES ##
-##############################
+###############################
+## EXPLORATORY DATA ANALYSES ##
+###############################
 
 # Univariate plots of orignal data -------------------------
 data %>%
